c342611d0ac2ad6ed8e35b32187b56ffec6f7b13
BigBug <mengdong@nancal.com>
2019-05-15 11:33:16 +0800

    edit base confirm

--- src/base/confirm/confirm.vue
+++ src/base/confirm/confirm.vue
@@ -1,13 +1,13 @@
     
 <template>
   <transition name="confirm-fade">
-    <div class="confirm" v-show="showFlag" @click.stop>
+    <div class="confirm" >
       <div class="confirm-wrapper">
         <div class="confirm-content">
           <p class="text">{{text}}</p>
           <div class="operate">
-            <div @click="cancel" class="operate-btn left">{{cancelBtnText}}</div>
-            <div @click="confirm" class="operate-btn">{{confirmBtnText}}</div>
+            <div  class="operate-btn left">{{cancelBtnText}}</div>
+            <div class="operate-btn">{{confirmBtnText}}</div>
           </div>
         </div>
       </div>
@@ -37,21 +37,7 @@
       }
     },
     methods: {
-      show () {
-        this.showFlag = true
-      },
-      hide () {
-        this.showFlag = false
-      },
-      cancel () {
-        this.hide()
-        this.$emit('cancel')
-      },
-      confirm () {
-        this.hide()
-        this.$emit('confirm')
-      }
-    }
+      
   }
 </script>
 
